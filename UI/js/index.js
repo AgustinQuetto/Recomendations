69702fb76e265dfa193966b886ee4b255a32752d
Agustín Quetto <agustinquetto@gmail.com>
2018-07-27 16:28:07 -0300

    Update index.js

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,11 +1,11 @@
-$(window).ready(function(){
+$(window).ready(() => {
 	var left, width, cont = "#q-cont";
 	$('span').click(function(){
 		$(this).css("background","#5a6");
 		$('.re').css("background","#d90");
 		next();
 	});
-	$('.re').click(function(){
+	$('.re').click(() => {
 		$(".op").css("background","#d90");
 		$('.re').css("background","#5a6");
 		$(cont).css("left", "0px");
